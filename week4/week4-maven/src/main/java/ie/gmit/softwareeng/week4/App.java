57af7932de17bc6d6fa70d99cb9d8434ebc74cc9
amcglynn <amcglynn@cisco.com>
2019-03-03 22:34:21 +0000

    Added maven project and ant project restructure

---
+++ week4/week4-maven/src/main/java/ie/gmit/softwareeng/week4/App.java
@@ -0,0 +1,7 @@
+package ie.gmit.softwareeng.week4;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from week 4!");
+    }
+}
---
+++ week4/week4-maven/src/test/java/ie/gmit/softwareeng/week4/Tests.java
@@ -0,0 +1,18 @@
+package ie.gmit.softwareeng.week4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class Tests {
+
+    @Test
+    public void test1() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void test2() {
+        assertTrue(true);
+    }
+}
